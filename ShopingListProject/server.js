bd2605da0845977741bda219d1e1cd1d1e8cdc3a
DebarunMitra <debarun96mitra@gmail.com>
2019-11-13 18:18:37 +0530

    router path added

--- ShopingListProject/server.js
+++ ShopingListProject/server.js
@@ -2,6 +2,9 @@ const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 
+//add routes
+const items=require('./routes/api/items');
+
 const app=express();
 
 //body parser middleware
@@ -13,6 +16,9 @@ const db=require('./config/keys').mongoURI;
 //db connection
 mongoose.connect(db, {useNewUrlParser: true,useUnifiedTopology: true})
 .then(()=>console.log('MongoDB connected...')).catch(err=>console.log('ERROR: '+err));
+
+//use routes
+app.use('/api/items',items);
 
 //set port
 const port=process.env.PORT || 5020;
