412ec51a571d7c4754343fff1f441040b2fd00d7
DebarunMitra <debarun96mitra@gmail.com>
2019-11-14 14:47:36 +0530

    shoplist added

--- ShopingListProject/clientside/src/App.js
+++ ShopingListProject/clientside/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
+import ShopingList from './components/ShopingList';
+
 
 function App() {
   return (
     <div className="App">
     <AppNavbar />
+    <ShopingList />
       <h1>Hello Debarun</h1>
     </div>
   );
