6bc93d1ede0a084d7c25bdd9c1d6fa6972a3c11e
DebarunMitra <debarun96mitra@gmail.com>
2019-11-14 14:47:16 +0530

    shop list added

--- ShopingListProject/clientside/src/components/ShopingList.js
+++ ShopingListProject/clientside/src/components/ShopingList.js
@@ -7,3 +7,38 @@ import {
 } from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import uuid from 'uuid';
+
+class ShopingList extends Component{
+  state={
+    items:[
+      {id:uuid(), name:'Egg'},
+      {id:uuid(), name:'Milk'},
+      {id:uuid(), name:'Bread'},
+      {id:uuid(), name:'Jam'},
+    ]
+  }
+
+  render(){
+    return (
+      <Container>
+        <Button color="dark"
+          style={{marginBottom:'2rem'}}
+          onClick={()=>{
+            const name=prompt('Enter Item');
+            if(name){
+              this.setState(state=>({
+                items:[...state.items,{id:uuid(),name:name}]
+              }));
+            }
+          }}>Add Items</Button>
+          <ListGroup>
+            <TransitionGroup className="shoping list">
+            {items}
+            </TransitionGroup>
+          </ListGroup>
+      </Container>
+    );
+  }
+}
+
+export default ShopingList;
