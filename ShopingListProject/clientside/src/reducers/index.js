97d4f67b389667c4b9f17ea422d03366eeb8ff56
DebarunMitra <debarun96mitra@gmail.com>
2019-11-17 06:27:46 +0530

    reducer modified

--- ShopingListProject/clientside/src/reducers/index.js
+++ ShopingListProject/clientside/src/reducers/index.js
@@ -1,6 +1,6 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import itemReducer from './itemReducer';
 
 export default combineReducers({
-  item:itemReducer
+  item: itemReducer
 });
