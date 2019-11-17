080da9d7b83a511a496600d2c39c8583d78baa98
DebarunMitra <debarun96mitra@gmail.com>
2019-11-17 21:15:01 +0530

    app navbar modified

--- ShopingListProject/clientside/src/components/AppNavbar.js
+++ ShopingListProject/clientside/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
 import {
   Collapse,
   Navbar,
@@ -9,37 +9,77 @@ import {
   NavLink,
   Container
 } from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import RegisterModal from './auth/RegisterModal';
+import LoginModal from './auth/LoginModal';
+import Logout from './auth/Logout';
 
-class AppNavbar extends Component{
-    //this.toggle=this.toggle.bind(this);//way to include custom method to constructor OR wo can use Arrow function also
-    state={
-      isOpen:false
-    }
-    toggle=()=>{
-      this.setState({
-        isOpen:!this.state.isOpen
-      });
-    }
+class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  };
 
-    render(){
-      return (
-        <div>
-          <Navbar color="dark" dark expand="sm" className="mb-5">
-            <Container>
-              <NavbarBrand href="#">ShopingList</NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                  <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav  className="ml-auto">
-                      <NavItem>
-                        <NavLink href="https://github.com/debarunmitra">GitHub</NavLink>
-                      </NavItem>
-                    </Nav>
-                  </Collapse>
-            </Container>
-          </Navbar>
-        </div>
-      );
-    }
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <Fragment>
+        <NavItem>
+          <span className='navbar-text mr-3'>
+            <strong>{user ? `Welcome ${user.name}` : ''}</strong>
+          </span>
+        </NavItem>
+        <NavItem>
+          <Logout />
+        </NavItem>
+      </Fragment>
+    );
+
+    const guestLinks = (
+      <Fragment>
+        <NavItem>
+          <RegisterModal />
+        </NavItem>
+        <NavItem>
+          <LoginModal />
+        </NavItem>
+      </Fragment>
+    );
+
+    return (
+      <div>
+        <Navbar color='dark' dark expand='sm' className='mb-5'>
+          <Container>
+            <NavbarBrand href='/'>ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className='ml-auto' navbar>
+                {isAuthenticated ? authLinks : guestLinks}
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default AppNavbar;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(AppNavbar);
