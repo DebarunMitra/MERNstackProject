0b0efdb8032ef2c99161a4d15f24ad8def2681b9
DebarunMitra <debarun96mitra@gmail.com>
2019-11-17 07:53:04 +0530

    additem place changed

--- ShopingListProject/clientside/src/actions/itemActions.js
+++ ShopingListProject/clientside/src/actions/itemActions.js
@@ -1,22 +1,27 @@
+import axios from 'axios';
+
 import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from './types';
 
-export const getItems=()=>{
-  return {
-    type: GET_ITEMS
-  };
-};
-
-export const deleteItem=id=>{
-  return {
-    type: DELETE_ITEM,
-    payload:id
-  };
+export const getItems=()=>dispatch=>{
+  dispatch(setItemsLoading());
+  axios.get('/api/items').then(res=>dispatch({
+    type:GET_ITEMS,
+    payload:res.data
+  }))
 };
 
 export const addItem=item=>{
   return {
     type: ADD_ITEM,
     payload:item
+  };
+};
+
+
+export const deleteItem=id=>{
+  return {
+    type: DELETE_ITEM,
+    payload:id
   };
 };
 
