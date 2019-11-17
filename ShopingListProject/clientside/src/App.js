18f927c8dbc730bf7a9f24aedb0b654c7f0d07c8
DebarunMitra <debarun96mitra@gmail.com>
2019-11-17 21:16:40 +0530

    app js modified

--- ShopingListProject/clientside/src/App.js
+++ ShopingListProject/clientside/src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/authActions';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AppNavbar from './components/AppNavbar';
-import ShopingList from './components/ShopingList';
-import ItemModal from './components/ItemModal';
-import {Container} from 'reactstrap'
 
-import {Provider} from 'react-redux';
-import store from './store';
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
 
-
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <Container>
-        <ItemModal />
-          <ShopingList />
-            <h1>Hello Debarun</h1>
+  render() {
+    return (
+      <Provider store={store}>
+        <div className='App'>
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
           </Container>
-      </div>
-   </Provider>
-  );
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
