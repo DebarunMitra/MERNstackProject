30782ee2782963053e47c9aa04117bc87fb3c366
DebarunMitra <debarun96mitra@gmail.com>
2019-11-17 06:22:18 +0530

    add item and delete item added

--- ShopingListProject/clientside/src/App.js
+++ ShopingListProject/clientside/src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShopingList from './components/ShopingList';
+import ItemModal from './components/ItemModal';
+import {Container} from 'reactstrap'
 
 import {Provider} from 'react-redux';
 import store from './store';
@@ -13,8 +15,11 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
+        <Container>
+        <ItemModal />
           <ShopingList />
             <h1>Hello Debarun</h1>
+          </Container>
       </div>
    </Provider>
   );
