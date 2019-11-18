1c8309bca401f056b067dc8353a9df958928ecc7
DebarunMitra <debarun96mitra@gmail.com>
2019-11-18 15:03:08 +0530

    some basic test methods written

--- JESTtesting/functions.test.js
+++ JESTtesting/functions.test.js
@@ -7,3 +7,10 @@ it('add 2+2 to equal 4',()=>{
 it('add 2.2+2.2 to equal 4.4',()=>{
   expect(functions.add(2.2,2.2)).toBe(4.4);
 });
+
+//CHECK FOR TRUTHY & FALSY VALUES
+//toBeNull matches only Null
+//toBeUndefined matches only Undefined
+//toBeDefined matches only opposit of toBeUndefined
+//toBeTruthy matches anything that an if statement treats as true
+//toBeFalsy matches anything that an if statement treats as false
