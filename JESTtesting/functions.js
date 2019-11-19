027b0a62535f3cc36316e2f19fd21ebde6f29c66
DebarunMitra <debarun96mitra@gmail.com>
2019-11-19 13:26:59 +0530

    user name added

--- JESTtesting/functions.js
+++ JESTtesting/functions.js
@@ -2,7 +2,12 @@ const functions={
   add:(num1,num2)=>num1+num2,
   isNull:()=>null,
   isUndefined:()=>undefined,
-  checkValue:(val)=>val
+  checkValue:(val)=>val,
+  createUser:()=>{
+    const user={firstName:"Debarun"};
+    user['lastName']="Mitra";
+    return user;
+  }
 };
 
 module.exports=functions;
