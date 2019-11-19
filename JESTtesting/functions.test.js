9e1918f5685cd3716ab44cb711a15f11fe6de32c
DebarunMitra <debarun96mitra@gmail.com>
2019-11-19 11:00:48 +0530

    module added

--- JESTtesting/functions.test.js
+++ JESTtesting/functions.test.js
@@ -14,3 +14,23 @@ it('add 2.2+2.2 to equal 4.4',()=>{
 //toBeDefined matches only opposit of toBeUndefined
 //toBeTruthy matches anything that an if statement treats as true
 //toBeFalsy matches anything that an if statement treats as false
+
+test('Should be null',()=>{
+  expect(functions.isNull()).toBeNull();
+});
+
+test('Should be undefined',()=>{
+  expect(functions.isUndefined()).toBeUndefined();
+});
+
+test('Should be FALSY',()=>{
+  expect(functions.isUndefined()).toBeUndefined();
+});
+
+test('Should be FALSY',()=>{
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test('Should be Truthy',()=>{
+  expect(functions.checkValue(1)).toBeTruthy();
+});
