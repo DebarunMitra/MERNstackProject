5594b9244df7f9618fe58c7c7cf28591ef1890ae
DebarunMitra <debarun96mitra@gmail.com>
2019-11-19 14:24:07 +0530

    regx and lessthan,greater than

--- JESTtesting/functions.test.js
+++ JESTtesting/functions.test.js
@@ -48,3 +48,17 @@ it('should be Debarun Mitra',()=>{
     lastName:'Mitra'
   })
 })
+
+//Less Than & greater than
+test('should be under 1600',()=>{
+  const load1=800;
+  const load2=700;
+  expect(load1+load2).toBeLessThan(1600);
+})
+
+//regx
+test('There is no D in the team',()=>{
+  expect('team').not.toMatch(/D/i);
+})
+
+//
