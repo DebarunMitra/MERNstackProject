4c048bfb6d5a188ad86f72745c248aaa02aef590
DebarunMitra <debarun96mitra@gmail.com>
2019-11-19 15:36:13 +0530

    promise test

--- JESTtesting/functions.js
+++ JESTtesting/functions.js
@@ -1,3 +1,4 @@
+const axios=require('axios');
 const functions={
   add:(num1,num2)=>num1+num2,
   isNull:()=>null,
@@ -7,7 +8,11 @@ const functions={
     const user={firstName:"Debarun"};
     user['lastName']="Mitra";
     return user;
-  }
+  },
+  fetchUser:()=>
+    axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res=>res.data)
+    .catch(err=>err)
 };
 
 module.exports=functions;
