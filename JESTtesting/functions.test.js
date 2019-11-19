4084f808f4bc73e24cfd6b45854dd5802715e113
DebarunMitra <debarun96mitra@gmail.com>
2019-11-19 13:27:14 +0530

    create user test added

--- JESTtesting/functions.test.js
+++ JESTtesting/functions.test.js
@@ -1,9 +1,11 @@
 const functions=require('./functions');
 
+//addition test 1
 it('add 2+2 to equal 4',()=>{
   expect(functions.add(2,2)).toBe(4);
 });
 
+//addition test 2
 it('add 2.2+2.2 to equal 4.4',()=>{
   expect(functions.add(2.2,2.2)).toBe(4.4);
 });
@@ -15,22 +17,34 @@ it('add 2.2+2.2 to equal 4.4',()=>{
 //toBeTruthy matches anything that an if statement treats as true
 //toBeFalsy matches anything that an if statement treats as false
 
+//null test
 test('Should be null',()=>{
   expect(functions.isNull()).toBeNull();
 });
 
+//undefined test
 test('Should be undefined',()=>{
   expect(functions.isUndefined()).toBeUndefined();
 });
 
+//falsy test
 test('Should be FALSY',()=>{
   expect(functions.isUndefined()).toBeUndefined();
 });
 
+//falsy test
 test('Should be FALSY',()=>{
   expect(functions.checkValue(null)).toBeFalsy();
 });
 
+//Truthy test
 test('Should be Truthy',()=>{
   expect(functions.checkValue(1)).toBeTruthy();
 });
+
+it('should be Debarun Mitra',()=>{
+  expect(functions.createUser()).toEqual({
+    firstName:'Debarun',
+    lastName:'Mitra'
+  })
+})
