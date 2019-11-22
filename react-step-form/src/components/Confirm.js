f2eeb2dced70168ef6ef26e8d5ee97f03b6e363a
DebarunMitra <debarun96mitra@gmail.com>
2019-11-22 16:16:06 +0530

    confirm working

--- react-step-form/src/components/Confirm.js
+++ react-step-form/src/components/Confirm.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
-import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import {List,ListItem} from 'material-ui/List';
 
