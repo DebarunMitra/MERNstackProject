451c5ca09d33c33e97e2ea277dcc1a2681c14c37
DebarunMitra <debarun96mitra@gmail.com>
2019-11-25 02:32:18 +0530

    button added

--- mern-authentication/client/src/components/Header.js
+++ mern-authentication/client/src/components/Header.js
@@ -6,11 +6,11 @@ render(){
   return (
     <nav>
     <div className="nav-wrapper blue darken-4">
-      <a href="#" className="brand-logo">WaterHut</a>
+      <a href="/" className="brand-logo">WaterHut</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#">Sign Up</a></li>
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">Logout</a></li>
+        <li><a href="/auth/google">Sign Up</a></li>
+        <li><a href="/profile">Profile</a></li>
+        <li><a href="/auth/logout">Logout</a></li>
       </ul>
     </div>
   </nav>
