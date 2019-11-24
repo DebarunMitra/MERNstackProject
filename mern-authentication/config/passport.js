2dd407deb2dbac860594def00cc2bfc7f659ca63
DebarunMitra <debarun96mitra@gmail.com>
2019-11-25 00:07:27 +0530

    passport added

---
+++ mern-authentication/config/passport.js
@@ -0,0 +1,46 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./keys');
+
+
+module.exports = function(passport){
+  passport.use(
+    new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret:keys.googleClientSecret,
+      callbackURL:'/auth/google/callback',
+      proxy: true
+    }, (accessToken, refreshToken, profile, done) => {
+      const newUser = {
+        googleID: profile.id,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        email: profile.emails[0].value,
+        image: profile.photos[0].value
+      }
+      console.log(newUser);
+      // Check for existing user
+      // User.findOne({
+      //   googleID: profile.id
+      // }).then(user => {
+      //   if(user){
+      //     // Return user
+      //     done(null, user);
+      //   } else {
+      //     // Create user
+      //     new User(newUser)
+      //       .save()
+      //       .then(user => done(null, user));
+      //   }
+      // })
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+  });
+}
