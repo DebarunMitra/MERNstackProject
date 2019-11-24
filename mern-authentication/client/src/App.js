134f23f526d593a2b7b690fd0938b86cb629ce86
DebarunMitra <debarun96mitra@gmail.com>
2019-11-25 02:02:23 +0530

    app.js modified

--- mern-authentication/client/src/App.js
+++ mern-authentication/client/src/App.js
@@ -2,24 +2,24 @@ import React, {
   Component
 } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "jquery/dist/jquery.min.js";
-import NavBar from './components/AppNavBar/NavBar';
-import Login from './components/LoginInterface/Login';
-
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import "jquery/dist/jquery.min.js";
+//import NavBar from './components/AppNavBar/NavBar';
+//import Login from './components/LoginInterface/Login';
+import Header from './components/Header';
+import {BrowserRouter,Route} from 'react-router-dom';
 
 //    <NavBar />
 
 class App extends Component {
 
   render() {
-    return ( <
-      div className = "App" >
-      <
-      Login / >
-      <
-      h6 > WaterHut < /h6> <
-      /div>
+    return (
+      <BrowserRouter>
+        <Header />
+        <Route path="/" />
+        <h6> WaterHut </h6>
+      </BrowserRouter>
     );
   }
 }
