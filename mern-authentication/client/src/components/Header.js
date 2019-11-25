789034c011d6f9fce197ba76f339f4f6af14283f
DebarunMitra <debarun96mitra@gmail.com>
2019-11-25 13:06:10 +0530

    header modified

--- mern-authentication/client/src/components/Header.js
+++ mern-authentication/client/src/components/Header.js
@@ -1,22 +1,49 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux';
 
 class Header extends Component{
 
 render(){
+  const rederContect = ()=>{
+    switch(this.props.user){
+            case null:
+              return (
+                <React.Fragment>
+                  <li><a href="/auth/verify">loading</a></li>
+                  <li><a href="/auth/google">signup</a></li>
+                </React.Fragment>
+          );
+            case false:
+                return  <li><a href="/auth/google">signup</a></li>
+            default:
+                 return(
+                     <React.Fragment>
+                       <li><a href="/auth/logout">logout</a></li>
+                       <li><Link to="/profile">profile</Link></li>
+                     </React.Fragment>
+                 )
+        }
+  };
+
   return (
-    <nav>
-    <div className="nav-wrapper blue darken-4">
-      <a href="/" className="brand-logo">WaterHut</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="/auth/google">Sign Up</a></li>
-        <li><a href="/profile">Profile</a></li>
-        <li><a href="/auth/logout">Logout</a></li>
-      </ul>
-    </div>
-  </nav>
+          <nav>
+             <div className="nav-wrapper deep-purple darken-2">
+             <Link to={this.props.user ? '/profile' : '/'} className="brand-logo">WaterHut</Link>
+             <ul id="nav-mobile" className="right">
+                {rederContect()}
+             </ul>
+             </div>
+         </nav>
   );
 }
 
 }
 
-export default Header;
+const mapStateToProps=(state)=>{
+  return {
+    user:state.auth
+  };
+}
+
+export default connect(mapStateToProps)(Header);
