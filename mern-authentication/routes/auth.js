5c004ed2ed6504d1247e16a3c73885329b0ad441
DebarunMitra <debarun96mitra@gmail.com>
2019-11-25 10:32:36 +0530

    auth modified

--- mern-authentication/routes/auth.js
+++ mern-authentication/routes/auth.js
@@ -6,15 +6,20 @@ router.get('/google', passport.authenticate('google', {scope: ['profile', 'email
 
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
-    res.redirect('/dashboard');
+    //res.redirect('/dashboard');
+    res.redirect('/');
   });
 
-  
+
+
   router.get('/verify', (req, res) => {
     if(req.user){
+      console.log('verify');
       console.log(req.user);
+      res.status(200).send(req.user);
     } else {
       console.log('Not Auth');
+            res.status(401).send('Not Auth');
     }
   });
 
