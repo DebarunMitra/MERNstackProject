f74e5d33810a235a555b0dcb2bc40ad86f2bfc5f
DebarunMitra <debarun96mitra@gmail.com>
2019-11-25 16:45:01 +0530

    profile page modified

--- mern-authentication/client/src/components/Profile.js
+++ mern-authentication/client/src/components/Profile.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Profile extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName:"loading",
+      image:""
+    };
+  }
+
+  componentDidMount() {
+    if(this.props.user){
+            this.setState({
+                firstName:this.props.user.firstName,
+                image:this.props.user.image
+       })
+     }
+   }
 
 render(){
+  if(!this.props.user){
+       this.props.history.push('/')
+   }
   return (
-    <h4>I am profile :)</h4>
+        <div>
+           <h1>here is your profile </h1>
+           <div className="card" style={{margin:"10%",padding:"10px",textAlign:"center"}}>
+              <h2>{this.state.firstName}</h2>
+              <img className="circle" src={this.state.image} alt=""/>
+           </div>
+        </div>
   );
 }
 
 }
 
-export default Profile;
+const mapStateToProps = (state)=>{
+   return {
+       user:state.auth
+   }
+}
+
+export default connect(mapStateToProps)(Profile);
