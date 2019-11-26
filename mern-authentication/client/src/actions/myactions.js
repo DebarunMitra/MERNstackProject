3956e144f40c9b6aea55291c2bf26b23f35bf77c
DebarunMitra <debarun96mitra@gmail.com>
2019-11-26 22:36:39 +0530

    alignment

--- mern-authentication/client/src/actions/myactions.js
+++ mern-authentication/client/src/actions/myactions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchUserAction = ()=>{
    return (dispatch)=>{
-
+     
     axios.get('/auth/verify')
     .then((res)=>{
        dispatch({type:'GET_USER',payload:res.data})
