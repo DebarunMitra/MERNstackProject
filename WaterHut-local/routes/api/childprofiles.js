5fa19e9c4cc3fab5ca16da91710ba927b60337e2
DebarunMitra <debarun96mitra@gmail.com>
2019-11-29 19:59:33 +0530

    routes discarded

--- WaterHut-local/routes/api/childprofiles.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-//@route  GET api/childprofiles
-//@access Public
-router.get("/", (req, res) => res.send("child profiless route"));
-
-module.exports = router;
